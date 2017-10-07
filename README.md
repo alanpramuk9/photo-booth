cda832c3cdd86181eec0e2196cd05dcfc27e636f
Chance Smith <chancesmithb@gmail.com>
2017-10-07 06:38:28 -0500

    adds successful image overlay test - w/o camera or new image creation from camera captures

--- README.md
+++ README.md
@@ -2,9 +2,19 @@
 Raspberry Pi Photo booth (PiCamera v2)
 
 ## setup
+1. begin setup on raspberry pi
 1. $`cd virtualenv && virtualenv env --not-site-packages` to prep all dependencies in project
-2. start vitural enviroment in `virtualenv/env/` folder $`source bin/activate`
-3. go fetch all python dependencies $`pip install -r requirements.txt`
+1. start vitural enviroment in `virtualenv/env/` folder $`source bin/activate` (see: http://sourabhbajaj.com/mac-setup/Python/virtualenv.html)
+1. go fetch all python dependencies $`pip install -r requirements.txt`
+1. setup picamera $`sudo apt-get install python3-setuptools && easy_install3 --user picamera` (see: https://media.readthedocs.org/pdf/picamera/release-0.8/picamera.pdf)
 
-## stop virtual enviroment
-- $`deactivate`
+## helpful tips with setup
+- connect mac to pi file system for editing (see: https://www.raspberrypi.org/documentation/remote-access/ssh/sshfs.md)
+- stop virtual enviroment $`deactivate`
+- freeze new dependencies while virtual enviroment is active (see setup #3) in `virtualenv/env/` folder $`pip freeze --local > requirements.txt`
+
+## virtualenv - virtual enviroment introduction
+- docs - http://sourabhbajaj.com/mac-setup/Python/virtualenv.html
+- https://www.youtube.com/watch?v=VgP0xzLeLgA
+- virtualenv, pip freeze, pip install >> https://www.youtube.com/watch?v=N5vscPTWKOk
+- moving to Docker >> https://www.youtube.com/watch?v=ETL-_W1W8gY
--- camera.py
+++ camera.py
@@ -1,27 +1,37 @@
 # pi photobooth
 #from picamera import PiCamera
 from time import sleep
+from datetime import datetime
 import uuid
-import Image
+from PIL import Image
 
-camera = PiCamera()
-id = uuid.uuid4()
-
+#camera = PiCamera()
+#id = uuid.uuid4()
+# static id for testing
+id = '94457bc8-a749-4afc-aa06-11c50bc942c0'
+# static image path for testing
+imageName = (
+    '/home/pi/wip/photo-booth/camera-shots/photo-0__94457bc8-a749-4afc-aa06-11c50bc942c0.jpg',
+    '/home/pi/wip/photo-booth/camera-shots/photo-1__94457bc8-a749-4afc-aa06-11c50bc942c0.jpg',
+    '/home/pi/wip/photo-booth/camera-shots/photo-2__94457bc8-a749-4afc-aa06-11c50bc942c0.jpg',
+    '/home/pi/wip/photo-booth/camera-shots/photo-3__94457bc8-a749-4afc-aa06-11c50bc942c0.jpg'
+    )
 
 #camera.start_preview()
 sleep(3)
 for i in range(4):
     sleep(1)
-    imageName = '/home/pi/wip/photo-booth/camera-shots/photo-'+str(i)+'__'+str(id)+'.jpg'
+    #imageName = '/home/pi/wip/photo-booth/camera-shots/photo-'+str(i)+'__'+str(id)+'.jpg'
     imageNameWithOverlay = '/home/pi/wip/photo-booth/camera-shots/overlay-photo-'+str(i)+'__'+str(id)+'.jpg'
-    print(imageName)
+    print(imageName[i])
     #camera.capture(imageName)
     # setup overlay images
-    background = Image.open(imageName)
+    background = Image.open(imageName[i])
     foreground = Image.open('/home/pi/wip/photo-booth/assets/overlay.png')
     # merge photos
     background.paste(foreground, (0, 0), foreground)
     background.save(imageNameWithOverlay)
+    # make gif
     print('round '+str(i))
 print('all done')
 #camera.stop_preview()
