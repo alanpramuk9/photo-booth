5709e9778e390525b0e144f14a8ac88d1d7141ac
Chance Smith <chancesmithb@gmail.com>
2017-10-07 06:09:11 -0500

    adds requierements.txt and setup instructions

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # photo-booth
 Raspberry Pi Photo booth (PiCamera v2)
+
+## setup
+1. $`cd virtualenv && virtualenv env --not-site-packages` to prep all dependencies in project
+2. start vitural enviroment in `virtualenv/env/` folder $`source bin/activate`
+3. go fetch all python dependencies $`pip install -r requirements.txt`
+
+## stop virtual enviroment
+- $`deactivate`
--- camera.py
+++ camera.py
@@ -1,5 +1,5 @@
 # pi photobooth
-from picamera import PiCamera
+#from picamera import PiCamera
 from time import sleep
 import uuid
 import Image
@@ -8,18 +8,20 @@ camera = PiCamera()
 id = uuid.uuid4()
 
 
-camera.start_preview()
+#camera.start_preview()
 sleep(3)
 for i in range(4):
     sleep(1)
     imageName = '/home/pi/wip/photo-booth/camera-shots/photo-'+str(i)+'__'+str(id)+'.jpg'
     imageNameWithOverlay = '/home/pi/wip/photo-booth/camera-shots/overlay-photo-'+str(i)+'__'+str(id)+'.jpg'
     print(imageName)
-    camera.capture(imageName)
+    #camera.capture(imageName)
     # setup overlay images
     background = Image.open(imageName)
     foreground = Image.open('/home/pi/wip/photo-booth/assets/overlay.png')
     # merge photos
     background.paste(foreground, (0, 0), foreground)
     background.save(imageNameWithOverlay)
-camera.stop_preview()
+    print('round '+str(i))
+print('all done')
+#camera.stop_preview()
