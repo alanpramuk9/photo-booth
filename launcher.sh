4d0ae27d3fc4204d8011739318930f8331edc3ac
Chance Smith <chancesmithb@gmail.com>
2017-10-22 21:00:34 -0500

    adds shopt [launcher.sh]

--- launcher.sh
+++ launcher.sh
@@ -4,17 +4,16 @@
 ###########################
 ## start photobooth
 ###########################
-function start_photo_booth() {
-  # source the new aliases added
-  . /home/pi/.bashrc
-  # start python virtual enviroment (`apb` is an alias  = `. ../.env/bin/activate`)
-  apb
-  # open browser (alias = chromium-browser --kiosk http://localhost)
-  opb
-}
 
-# vroom vroom - start function
-start_photo_booth
+# shopt to add interactive shell
+shopt -s expand_aliases
+# source the new aliases added
+. /home/pi/.bashrc
+# start python virtual enviroment (`apb` is an alias  = `. ../.env/bin/activate`)
+apb
+# open browser (alias = chromium-browser --kiosk http://localhost)
+opb
+
 ###########################
 ## /END start photobooth
 ###########################
