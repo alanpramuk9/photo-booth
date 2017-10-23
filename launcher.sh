56fb266cbf244b2546635d1a5ff3c65f6d87edd0
Chance Smith <chancesmithb@gmail.com>
2017-10-22 19:50:18 -0500

    adds sleep to chromium launch [launcher]

--- launcher.sh
+++ launcher.sh
@@ -7,6 +7,8 @@
 # start python virtual enviroment
 cd ~/wip/photo-booth/
 source virtualenv/envpi2/bin/activate
+# sleep for a bit (5 seconds)
+sleep 5
 # open browser
 chromium-browser --kiosk http://localhost
 
