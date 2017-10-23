c5eece2ee24c4e10976cd2163f9386b07c9d5982
Chance Smith <chancesmithb@gmail.com>
2017-10-22 20:32:25 -0500

    adds source of aliases [lanucher.sh]

--- launcher.sh
+++ launcher.sh
@@ -4,6 +4,8 @@
 ## start photobooth
 ###########################
 
+# source the new aliases added
+. ~/.bashrc
 # start python virtual enviroment (`apb` is an alias  = `. ../.env/bin/activate`)
 apb
 # open browser (alias = chromium-browser --kiosk http://localhost)
