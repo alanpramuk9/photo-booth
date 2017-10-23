67f8eb7fdf4aea3f62e26e0f42dd0a2c28481b0b
Chance Smith <chancesmithb@gmail.com>
2017-10-22 20:49:45 -0500

    try interactive shell with a function added [launcher.sh]

--- launcher.sh
+++ launcher.sh
@@ -4,14 +4,17 @@
 ###########################
 ## start photobooth
 ###########################
+function start_photo_booth() {
+  # source the new aliases added
+  . /home/pi/.bashrc
+  # start python virtual enviroment (`apb` is an alias  = `. ../.env/bin/activate`)
+  apb
+  # open browser (alias = chromium-browser --kiosk http://localhost)
+  opb
+}
 
-# source the new aliases added
-. /home/pi/.bashrc
-# start python virtual enviroment (`apb` is an alias  = `. ../.env/bin/activate`)
-apb
-# open browser (alias = chromium-browser --kiosk http://localhost)
-opb
-
+# vroom vroom - start function
+start_photo_booth
 ###########################
 ## /END start photobooth
 ###########################
